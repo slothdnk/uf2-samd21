16ce2842387d51605e70aee050fe3248315878ad
sabas1080 <s@theinventorhouse.org>
2019-03-21 15:23:14 -0600

    Add catwan citizen

---
+++ boards/catwan-citizen/board_config.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Electronic Cats SAPI de CV"
+#define PRODUCT_NAME "CatWAN Citizen"
+#define VOLUME_LABEL "CITIZEN"
+#define INDEX_URL "https://electroniccats.com/"
+#define BOARD_ID "SAMD21E18A-CatWAN-Citizen-v0"
+
+#define USB_VID 0x1209
+#define USB_PID 0xBAB4
+
+#define LED_PIN PIN_PA11
+
+#endif
