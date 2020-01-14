5f6352871ab0261df1293dbca07633bdbf3bda5b
sabas1080 <s@theinventorhouse.org>
2020-01-14 17:57:19 -0600

    init farmer

---
+++ boards/catwan_farmer/board_config.h
@@ -0,0 +1,19 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Electronic Cats SAPI de CV"
+#define PRODUCT_NAME "CatWAN Farmer"
+#define VOLUME_LABEL "FARMER"
+#define INDEX_URL "http://electroniccats.com"
+
+#define USB_VID 0x03EB   // Atmel
+#define USB_PID 0x2402   // Generic HID device
+
+#define BOARD_ID "Catwan-Farmer"
+
+#define LED_PIN PIN_PA08
+
+
+#endif
