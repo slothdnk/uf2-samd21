bbf90f30379bc0bdf6119aa79fbddf0aa492046c
Eduardo <werowero.edy@gmail.com>
2020-02-16 13:26:41 -0600

    add basic bast pro mini example and tracker

---
+++ boards/bastpro/board_config.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Electronic Cats SAPI de CV"
+#define PRODUCT_NAME "Bast Pro Mini M0"
+#define VOLUME_LABEL "Bast"
+#define INDEX_URL "http://electroniccats.com"
+#define BOARD_ID "bastpro"
+
+#define USB_VID 0x1209
+#define USB_PID 0xBAB3
+
+#define LED_PIN PIN_PA14
+
+#endif
---
+++ boards/catwan-tracker/board_config.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Electronic Cats SAPI de CV"
+#define PRODUCT_NAME "CatWAN Tracker"
+#define VOLUME_LABEL "Tracker"
+#define INDEX_URL "http://electroniccats.com"
+#define BOARD_ID "catwan-tracker"
+
+#define USB_VID 0x1209
+#define USB_PID 0xBAB9
+
+#define LED_PIN PIN_PA00
+
+#endif
